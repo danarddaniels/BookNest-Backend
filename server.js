9680cee1f1bfb3bb25ebcd41cfb2eda0318b28ed
Danard Daniels <danarddaniels@gmail.com>
2026-04-24 01:23:25 -0500

    Fix CORS origin

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: 'book-nest-sandy.vercel.app', // temporary for testing
+		origin: process.env.FRONTEND_URL, 
 		credentials: true,
 	}),
 );
