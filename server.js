3d1e3b0a9e1a14488495400a8b194c563c4509b2
Danard Daniels <danarddaniels@gmail.com>
2026-04-23 23:05:10 -0500

    overwrite

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const connectDB = require('./db');
 const cors = require('cors');
 const app = express();
 const UserRouter = require('./User');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(
